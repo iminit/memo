0861f8df4991cc97f19f3be4e52c80cde1d17481
imzdx <imzdx@qq.com>
2018-03-23 13:44:50 +0800

    添加时间差计算

--- src/main/java/com/iminit/index/IndexController.java
+++ src/main/java/com/iminit/index/IndexController.java
@@ -7,9 +7,12 @@ import com.jfinal.core.Controller;
 import com.jfinal.kit.Kv;
 import com.jfinal.kit.StrKit;
 import com.jfinal.log.Log;
+import com.xiaoleilu.hutool.date.DateUtil;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -107,6 +110,93 @@ public class IndexController extends Controller {
             renderJson(wordSentenceDao.wordsSentenceList(id, word));
         }
     }
+
+    public static class DayCompare {
+        private int year;
+        private int month;
+        private int day;
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public int getMonth() {
+            return month;
+        }
+
+        public void setMonth(int month) {
+            this.month = month;
+        }
+
+        public int getDay() {
+            return day;
+        }
+
+        public void setDay(int day) {
+            this.day = day;
+        }
+
+        public DayCompare(int year, int month, int day) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        @Override
+        public String toString() {
+            return "DayCompare{" +
+                    "year=" + year +
+                    ", month=" + month +
+                    ", day=" + day +
+                    '}';
+        }
+    }
+
+    /**
+     * 计算2个日期之间相差的  相差多少年月日
+     * 比如：2011-02-02 到  2017-03-02 相差 6年，1个月，0天
+     *
+     * @param fromDate
+     * @param toDate
+     * @return
+     */
+    public static DayCompare dayComparePrecise(Date fromDate, Date toDate) {
+        Calendar from = Calendar.getInstance();
+        from.setTime(fromDate);
+        Calendar to = Calendar.getInstance();
+        to.setTime(toDate);
+        int fromYear = from.get(Calendar.YEAR);
+        int fromMonth = from.get(Calendar.MONTH);
+        int fromDay = from.get(Calendar.DAY_OF_MONTH);
+        int toYear = to.get(Calendar.YEAR);
+        int toMonth = to.get(Calendar.MONTH);
+        int toDay = to.get(Calendar.DAY_OF_MONTH);
+        int year = toYear - fromYear;
+        int month = toMonth - fromMonth;
+        int day = toDay - fromDay;
+        return new DayCompare(year, month, day);
+    }
+
+    public void time() {
+
+    }
+
+    public void timeDiff() {
+        try {
+            String t1 = getPara("t1");
+            String t2 = getPara("t2");
+            DayCompare dayCompare = dayComparePrecise(DateUtil.parse(t1,"yyyy/MM/dd"), DateUtil.parse(t2,"yyyy/MM/dd"));
+            renderJson(dayCompare);
+        }catch (Exception e){
+            e.printStackTrace();
+            renderJson(e.getMessage());
+        }
+    }
+
 }
 
 
